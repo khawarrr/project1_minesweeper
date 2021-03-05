b4643290f1fb0098e020caf7848324042f4c22c5
khawarrr <mkhawar0907@gmail.com>
2021-03-05 13:57:14 -0800

    first commit

---
+++ js/main.js
@@ -0,0 +1,44 @@
+
+
+/*----- constants -----*/
+
+const bombCount = 15;
+ /*----- app's state (variables) -----*/
+
+ let grid, winner;
+
+
+
+  /*----- cached element references -----*/
+
+
+  const gridEl = document.getElementById('grid');
+
+  const resetBtn = document.getElementById('reset');
+
+
+
+  /*----- event listeners -----*/
+resetBtn.addEventListener('click', init);
+
+boardEl.addEventListener('click', handleClick);
+
+
+
+/*----- functions -----*/
+init();
+
+function init(){
+    winner = null;
+    render();
+}
+
+function render() {
+
+    const bombsArr = Array(bombCount).fill('bomb');
+
+    // render the grid
+    grid.forEach(function (boxVal, boxIdx) {
+        document.getElementById(boxIdx).innerHTML = playerIcon[boxVal];
+    });
+}
